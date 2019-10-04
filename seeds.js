5ff7d65b800c13cc3823c6478cf846eb4338cf4c
Oliver Day <oliver.day@usc.edu>
2019-10-03 18:42:46 -0700

    refactor seed.js callbacks with async/await

--- seeds.js
+++ seeds.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const Comment   = require("./models/comment");
  
-let data = [
+let seeds = [
     {
         name: "Cloud's Rest", 
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
@@ -18,49 +18,27 @@ let data = [
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     }
-]
+];
  
-function seedDB(){
-   //Remove all campgrounds
-   Campground.deleteMany({}, function(err){
-        if(err){
-            console.log(err);
-        }
-        console.log("removed campgrounds!");
-        Comment.deleteMany({}, function(err) {
-            if(err){
-                console.log(err);
-            }
-            console.log("removed comments!");
-             
-            //add a few campgrounds
-            data.forEach(function(seed){
-                Campground.create(seed, function(err, campground){
-                    if(err){
-                        console.log(err)
-                    } else {
-                        console.log("added a campground");
-                        
-                        //create a comment
-                        Comment.create(
-                            {
-                                text: "This place is great, but I wish there was internet",
-                                author: "Homer"
-                            }, function(err, comment){
-                                if(err){
-                                    console.log(err);
-                                } else {
-                                    campground.comments.push(comment);
-                                    campground.save();
-                                    console.log("Created new comment");
-                                }
-                            });
-                    }
+ async function seedDB(){
+   
+    try {
+        await Comment.deleteMany({});
+        await Campground.deleteMany({});
+
+        for(const seed of seeds) {
+            let campground = await Campground.create(seed);
+            let comment = await Comment.create(
+                {
+                    text: "This place is great, but I wish there was internet",
+                    author: "Homer"
                 });
-            });
-        });
-    }); 
-    //add a few comments
+                campground.comments.push(comment);
+                campground.save();
+        }
+    } catch(err) {
+        console.log(err);
+    }
 }
  
 module.exports = seedDB;
