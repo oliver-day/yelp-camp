818594b8aa1256a707e4664bc80cefedfb4fd622
Oliver Day <oliver.day@usc.edu>
2019-10-06 15:22:16 -0700

    add ability to connect to prod DB and fix sign up page

--- app.js
+++ app.js
@@ -21,10 +21,22 @@ const indexRoutes = require('./routes/index');
 
 
 // CONFIGURATION
-mongoose.connect("mongodb://localhost:27017/yelp_camp", 
-    {useNewUrlParser: true,
-     useUnifiedTopology: true
+
+// Local DB
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", 
+//     {useNewUrlParser: true,
+//      useUnifiedTopology: true
+// });
+
+// Production DB
+const uri = `${process.env.DB_HOST_PROD}${process.env.DB_USER_PROD}${process.env.DB_PASSWORD_PROD}${process.env.DB_NAME_PROD}`;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
